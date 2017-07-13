1c34aa424b7100abab3936b6846252eb47bc0c70
Chris Drain <DrainC4@yahoo.com>
2017-07-13 00:51:16 -0700

    Learned about transactions for multiple queries

--- inc.phpLogic/signup.php
+++ inc.phpLogic/signup.php
@@ -2,7 +2,7 @@
 include ('grievance.php');
 
 $employeeID = $_POST['eid'];
-$fullName = $_POST['full-name'];
+$fullName = $_POST['fullName'];
 $employeeType = $_POST['employeeStatus'];
 $address = $_POST['address'];
 $city = $_POST['city'];
@@ -25,26 +25,40 @@ $hash = password_hash($password, PASSWORD_DEFAULT);
 /*$sqlQueryCreateAccount = "INSERT INTO userAccounts (employeeID, emailAddress, PASSWORD) VALUES (?,?,?)";
 $sqlQueryUserSignUp = "INSERT INTO UserSignUp(fullName , employeeType , address, city , state, zipcode, phoneNumber,
 seniorityDate, payLevel, payStep, tour, daysOff, veteranStatus, layOffProtected) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)"; */
-$stmt = $conn->prepare("INSERT INTO userAccounts ( emailAddress, PASSWORD) VALUES (?,?,?)");
+$conn->beginTransaction();
+try{
+$stmt = $conn->prepare("INSERT INTO userAccounts ( emailAddress, PASSWORD) VALUES (?,?)");
 
 $stmt->bindValue(1,$email);
 $stmt->bindValue(2,$hash);
 $stmt->execute();
-$stmtSignUpInfo = $conn->prepare("INSERT INTO UserSignUp (fullName , employeeType , address, city , state, zipcode, phoneNumber,
-seniorityDate, payLevel, payStep, tour, daysOff, veteranStatus, layOffProtected) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-$stmtSignUpInfo->bindValue(1, $fullName);
-$stmtSignUpInfo->bindValue(2, $employeeType);
-$stmtSignUpInfo->bindValue(3, $address);
-$stmtSignUpInfo->bindValue(4, $city);
-$stmtSignUpInfo->bindValue(5, $state);
-$stmtSignUpInfo->bindValue(6, $zipCode);
-$stmtSignUpInfo->bindValue(7, $phone);
-$stmtSignUpInfo->bindValue(8,$seniority);
-$stmtSignUpInfo->bindValue(9, $payStatus);
-$stmtSignUpInfo->bindValue(10, $payStep);
-$stmtSignUpInfo->bindValue(11, $tour);
-$stmtSignUpInfo->bindValue(12, $daysOff);
-$stmtSignUpInfo->bindValue(13, $veteran);
-$stmtSignUpInfo->bindValue(14, $layOffProtected);
+$stmtCreateUnique = $conn->prepare("CREATE TABLE ".$employeeID."Grievances like filedGrievances");
+$stmtCreateUnique->execute();
+$stmtSignUpInfo = $conn->prepare("INSERT INTO UserSignUp (employeeID, fullName , employeeType , address, city , state, zipcode, phoneNumber,
+seniorityDate, payLevel, payStep, tour, daysOff, veteranStatus, layOffProtected) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+$stmtSignUpInfo->bindValue(1, $employeeID);
+$stmtSignUpInfo->bindValue(2, $fullName);
+$stmtSignUpInfo->bindValue(3, $employeeType);
+$stmtSignUpInfo->bindValue(4, $address);
+$stmtSignUpInfo->bindValue(5, $city);
+$stmtSignUpInfo->bindValue(6, $state);
+$stmtSignUpInfo->bindValue(7, $zipCode);
+$stmtSignUpInfo->bindValue(8, $phone);
+$stmtSignUpInfo->bindValue(9, $seniority);
+$stmtSignUpInfo->bindValue(10, $payStatus);
+$stmtSignUpInfo->bindValue(11, $payStep);
+$stmtSignUpInfo->bindValue(12, $tour);
+$stmtSignUpInfo->bindValue(13, $daysOff);
+$stmtSignUpInfo->bindValue(14, $veteran);
+$stmtSignUpInfo->bindValue(15, $layOffProtected);
 $stmtSignUpInfo->execute();
+$conn->commit();
+}
+catch(PDOException $e) {
+   echo "We have an error"."<br>";
+  echo $e->getMessage()."<br>";
+  $conn->rollBack();
+
+   }
+     $conn = null;
 header('Location:../userspage.html');
