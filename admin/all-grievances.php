b12fa74025de22098412cef96de3bef01539a3fc
jWatsonDev <jaywatson1982@gmail.com>
2017-08-02 10:52:48 +0000

    add comment. view all comments

--- admin/all-grievances.php
+++ admin/all-grievances.php
@@ -12,16 +12,28 @@ if ($_SESSION['admin']) {
 // Query database for name and email from 
 $query = $handler->query("SELECT * FROM filed_grievances");
 
+if ($_POST['submit-comment']) {
+  $topic = $_POST['topic'];
+  $comment = $_POST['comment'];
+  $grievance_id = $_POST['id'];
 
+  try {
+    $stmt = $handler->prepare("INSERT INTO comments (name, topic, comment, account_information_id) VALUES (?, ?, ?, ?)");
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $topic);
+    $stmt->bindValue(3, $comment);
+    $stmt->bindValue(4, $grievance_id);
+    $stmt->execute();
+    echo "Inserted";
+  } catch (PDOException $e) {
+    echo "We have an error: " . $e->getMessage() . "<br>";
+  }
+  
+}
 
 /*
  * Function format date
  */
-/*
-function formatDate($date) {
-  return date("M. j, Y, g:i A", strtotime($date));
-}
-*/
 function formatDate($date) {
   return date("M. j, Y", strtotime($date));
 }
@@ -69,8 +81,9 @@ function formatDate($date) {
                 <i class="fa fa-pencil-square-o fa-1x fa-panel" aria-hidden="true"></i> &nbsp; Submitted
               </td>
               <td>
-                <i class="fa fa-plus-square-o fa-2x fa-panel" aria-hidden="true" id="add-comment"></i>&nbsp;
-                <i class="fa fa-comment-o fa-2x fa-panel" aria-hidden="true"></i>
+                <i class="fa fa-plus-square-o fa-2x fa-panel postButton" aria-hidden="true" id="add-comment" data-key="<?php echo $row->id; ?>" onclick="getIdCreateComment(this)"></i>&nbsp;
+                <i class="fa fa-comment-o fa-2x fa-panel view-comments" aria-hidden="true" data-id="<?php echo $row->id; ?>" id=""></i>
+                <input type="hidden" name="postId" value="3">
               </td>
             </tr>
             <?php
@@ -100,12 +113,12 @@ function formatDate($date) {
       </div>
     </footer>
     
-    
     <div class="overlay"></div>
-    <div class="comment-form">
     
+<!--START OF COMMENT SUBMISSION FORM-->
+    <div class="comment-form">
       <!--START OF FORM - tabbed left for spacing-->
-      <form id="sign-up-form" method="post" action="add-acct-info.php">
+      <form id="sign-up-form" method="post" action="all-grievances.php">
         <h3 class="center-text">Add Comment</h3><br>
         <div class="row"> <!--FORM ROW--> 
           <div class="twelve columns">
@@ -114,23 +127,56 @@ function formatDate($date) {
           </div>
           <div class="error" id = "full-name-error">Full Name Required</div>
         </div> <!--END ROW-->
-      
         <div class="row"> <!--FORM ROW--> 
           <div class="twelve columns">
             <label for="comment">Comment</label>
-            <textarea class="u-full-width" placeholder="Comment here..." id="comment"></textarea>
+            <textarea class="u-full-width" placeholder="Comment here..." id="comment" name="comment"></textarea>
           </div>
         </div> <!--END ROW-->
-
-        <input id="submit" type="submit" value="Add Comment" class="submit-button" name="submit">
+        <input type="hidden" id="setId" value="" name="id">
+        <input id="submit" type="submit" value="Add Comment" class="submit-button" name="submit-comment">
       </form>
       <!--END OF FORM - tabbed left for spacing-->
     </div>
+<!--END OF COMMENT SUBMISSION FORM-->
      
-    
-    
+<!--START OF VIEW COMMENTS-->
+    <div class="view-related-comments">
+      <?php $query = $handler->query("SELECT * FROM comments"); ?>
+      <div>
+        <h3 class="center-text">Comment/s</h3><br>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php while ($row = $query->fetch(PDO::FETCH_OBJ)) : ?>
+            <tr>
+              <td><?php echo $row->name; ?></td>
+              <td><?php echo $row->comment; ?></td>
+            </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+<!--END OF VIEW COMMENTS-->
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+      /*
+       * gets data-key of comment that is clicked on
+       * Will move into main script later
+       */
+      function getIdCreateComment(e){
+        var theId = e.getAttribute("data-key");
+        document.getElementById('setId').value = theId;
+        createComment();
+      }
+    </script>
     <script src="../js/script.js"></script>
   </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -16,18 +16,27 @@ $("#change-email, .create-account").click(function() {
     });
 });
 
-$("#add-comment").click(function() {
+$(".view-comments").click(function() {
     $(".overlay").fadeIn();
-    $(".comment-form").fadeIn("slow");
+    $(".view-related-comments").fadeIn("slow");
     $('html, body').css({
         overflow: 'hidden',
         height: '100%'
     });
 });
 
+function createComment() {
+    $(".overlay").fadeIn();
+    $(".comment-form").fadeIn("slow");
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
+}
+
 
 $('.overlay').on('click', function(event) {
-    $(".overlay, .registration-form, .update-email-form, .comment-form").fadeOut("slow");
+    $(".overlay, .registration-form, .update-email-form, .comment-form, .view-related-comments").fadeOut("slow");
     $('html, body').css({
         overflow: 'auto',
         height: 'auto'
